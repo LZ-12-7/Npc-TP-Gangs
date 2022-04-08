e1e7aa5455fd8bdd58328e00f7e59984c7836975
LZ <94126308+LZ-12-7@users.noreply.github.com>
2022-04-08 23:22:21 +0200

    Delete locales directory

--- locales/en.lua
+++
@@ -1,6 +0,0 @@
-Locales['en'] = {
-    ['vagos_message'] = 'You have been teleported to the vagos hood.',
-    ['ballas_message'] = 'You have been teleported to the ballas hood',
-    ['whitefamilies_message'] = 'You have been teleported to the whitefamilies hood',
-    ['families_message'] = 'You have been teleported to the families hood',
-}
--- locales/es.lua
+++
@@ -1,6 +0,0 @@
-Locales['es'] = {
-    ['vagos_message'] = 'has sido Tpeado al barrio de los vagos',
-    ['ballas_message'] = 'has sido Tpeado al barrio de los ballas',
-    ['whitefamilies_message'] = 'has sido Tpeado al barrio de los white families',
-    ['families_message'] = 'has sido Tpeado al barrio de los families',
-}
